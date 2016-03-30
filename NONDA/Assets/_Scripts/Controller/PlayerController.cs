4590289ee7db165de69c3a83808241fb698260af
Elias de M. Fernandes <eliasdemoraes@live.com>
2016-03-30 11:09:11 -0300

    Alterations to the file, using MVC

--- NONDA/Assets/_Scripts/Controller/PlayerController.cs
+++ NONDA/Assets/_Scripts/Controller/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rigidB2D;
 	private bool grounded;
 	private bool doubleJumped;
+	private bool facingRight;
 	private Animator animator;
 	#endregion
 
@@ -21,56 +22,53 @@ public class PlayerController : MonoBehaviour {
 	#endregion
 
 	void Start () {
-		player = FindObjectOfType<Player>();
-		animator = GetComponent<Animator>();
-		rigidB2D = gameObject.GetComponent<Rigidbody2D>();
-		rigidB2D.freezeRotation = true;
-	}
-
-	void Update () {
-		if(grounded)
-			doubleJumped = false;
-
-		animator.SetBool("Grounded", grounded);
-		
-		if(Input.GetKeyDown (KeyCode.Space) && grounded){
-			Jump();
-		}
-		if(Input.GetKeyDown (KeyCode.Space) && !doubleJumped && !grounded){
-			DoubleJump();
-		}
-		if(Input.GetKeyDown (KeyCode.RightArrow)){
-			rigidB2D.velocity = new Vector2 (player.MoveSpeed, rigidB2D.velocity.y);
-		}
-		if(Input.GetKeyDown (KeyCode.LeftArrow)){
-			rigidB2D.velocity = new Vector2 (-player.MoveSpeed, rigidB2D.velocity.y);
-		}
-
-		animator.SetFloat("Speed", Mathf.Abs (rigidB2D.velocity.x));
-		Flip();
-	}
-
-	void Flip(){
-		if(rigidB2D.velocity.x > 0){
-			transform.localScale = new Vector3(1f, 1f, 1f);
-		}else if(rigidB2D.velocity.x < 0){
-			transform.localScale = new Vector3(-1f, 1f, 1f);
-		}
-	}
-
-	void Jump(){
-		rigidB2D.velocity = new Vector2 (rigidB2D.velocity.x, player.JumpHeight);
-	}
-
-	void DoubleJump(){
-		rigidB2D.velocity = new Vector2 (rigidB2D.velocity.x, player.JumpHeight);
-		doubleJumped = true;
+	    facingRight = true;
+	    player = FindObjectOfType<Player>();
+	    animator = GetComponent<Animator>();
+	    rigidB2D = gameObject.GetComponent<Rigidbody2D>();
+	    rigidB2D.freezeRotation = true;
 	}
 
 	void FixedUpdate(){
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	    grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	    float horizontal = Input.GetAxis("Horizontal");
 
-//		float h = Input.GetAxis("Horizontal");
-//		rigidB2D.AddForce ((Vector2.right * moveSpeed) * h);
+	    if(grounded)
+	        doubleJumped = false;
+
+	    animator.SetBool("Grounded", grounded);
+
+	    if(Input.GetKeyDown (KeyCode.Space) && grounded){
+	        Jump();
+	    }
+	    if(Input.GetKeyDown (KeyCode.Space) && !doubleJumped && !grounded){
+	        DoubleJump();
+	    }
+
+	    HandleMovement(horizontal);
+	    FlipHorizontal(horizontal);
+	}
+
+	private void HandleMovement(float horizontal){
+	    rigidB2D.velocity = new Vector2(horizontal * player.MoveSpeed, rigidB2D.velocity.y);
+	    animator.SetFloat("Speed", Mathf.Abs (horizontal));
+	}
+
+	private void FlipHorizontal(float horizontal){
+	    if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight){
+	        facingRight = !facingRight;
+	        Vector3 flipScale = transform.localScale;
+	        transform.localScale = flipScale;
+	        Debug.Log("Entry here to flip");
+	    }
+	}
+
+	void Jump(){
+	    rigidB2D.velocity = new Vector2 (rigidB2D.velocity.x, player.JumpHeight);
+	}
+
+	void DoubleJump(){
+	    rigidB2D.velocity = new Vector2 (rigidB2D.velocity.x, player.JumpHeight);
+	    doubleJumped = true;
 	}
 }
