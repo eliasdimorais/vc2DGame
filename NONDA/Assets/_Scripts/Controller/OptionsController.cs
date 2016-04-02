3f93ac7040063b1603aae38689e3687f40117742
Elias de M. Fernandes <eliasdemoraes@live.com>
2016-04-01 23:28:50 -0300

    * Nonda.userprefs: * 01a_Start.unity: * 02Level_01.unity: * TagManager.asset: * Game Canvas.prefab: * mask@2x.png.meta: * ProjectVersion.txt: * frame@2x.png.meta: * points@2x.png.meta: * navbar@2x.png.meta: * ProjectSettings.asset: * lifebar@2x.png.meta: * UnityAdsSettings.asset: * GraphicsSettings.asset: * icon_star@2x.png.meta: * icon_star@2x2.png.meta: * heart_icon@2x.png.meta: * menu_button@2x.png.meta: * UnityConnectSettings.asset: * MusicManager.cs: * PlayerController.cs: * OptionsController.cs: * bird2_walk.png.meta: * bird2_idle.png.meta: * bird2_walk2.png.meta:
    * Assembly-CSharp.csproj: Added movement to the player and collisor on
      the wall working after 13 long hours setting up and figuring out

---
+++ NONDA/Assets/_Scripts/Controller/OptionsController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+	#region Public Variables
+	public Slider volumeSlider;
+	public LevelManager levelManager;
+	#endregion
+
+	#region Private Variabless
+	//private MusicManager musicManager;
+	#endregion
+
+	void Start(){
+		//musicManager = GameObject.FindObjectOfType<MusicManager>();
+	}
+
+	public void SaveOptionsAndExit(){
+		//PlayerPrefsManager.SetMasterVolume(volumeSlider);
+		//levelManager.LoadLevel("01a_Start");
+	}
+}
+
+
--- NONDA/Assets/_Scripts/Controller/PlayerController.cs
+++ NONDA/Assets/_Scripts/Controller/PlayerController.cs
@@ -2,73 +2,121 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	#region Public Variables
+	#region Public Ground Variables
 	public Transform groundCheck;
-	public float groundCheckRadius;
+	public float groundRadius;
 	public LayerMask whatIsGround;
+	public float facingRight;
 	#endregion
 
 	#region Private Variables
 	private Rigidbody2D rigidB2D;
-	private bool grounded;
-	private bool doubleJumped;
-	private bool facingRight;
+	private bool grounded = false;
+	private bool doubleJumped = false;
 	private Animator animator;
-	#endregion
 
+	//private Vector2 touchOrigin = -Vector2.one;
+	#endregion
 
 	#region Instances 
 	public Player player;
 	#endregion
 
 	void Start () {
-	    facingRight = true;
 	    player = FindObjectOfType<Player>();
 	    animator = GetComponent<Animator>();
-	    rigidB2D = gameObject.GetComponent<Rigidbody2D>();
+	    rigidB2D = GetComponent<Rigidbody2D>();
 	    rigidB2D.freezeRotation = true;
+
 	}
 
-	void FixedUpdate(){
-	    grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-	    float horizontal = Input.GetAxis("Horizontal");
-
-	    if(grounded)
-	        doubleJumped = false;
-
-	    animator.SetBool("Grounded", grounded);
-
-	    if(Input.GetKeyDown (KeyCode.Space) && grounded){
+	void Update(){
+	    if(Input.GetButtonDown("Jump") && grounded){
 	        Jump();
 	    }
-	    if(Input.GetKeyDown (KeyCode.Space) && !doubleJumped && !grounded){
+
+	    if(Input.GetButtonDown("Jump") && !doubleJumped && !grounded){
 	        DoubleJump();
 	    }
-
-	    HandleMovement(horizontal);
-	    FlipHorizontal(horizontal);
 	}
 
-	private void HandleMovement(float horizontal){
-	    rigidB2D.velocity = new Vector2(horizontal * player.MoveSpeed, rigidB2D.velocity.y);
-	    animator.SetFloat("Speed", Mathf.Abs (horizontal));
+	void RotateLeft() {
+    	transform.Rotate (Vector3.back * -5f);
 	}
 
-	private void FlipHorizontal(float horizontal){
-	    if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight){
-	        facingRight = !facingRight;
-	        Vector3 flipScale = transform.localScale;
-	        transform.localScale = flipScale;
-	        Debug.Log("Entry here to flip");
-	    }
+	void RotateRight() {
+    transform.Rotate (Vector3.forward * 5f);
+}
+
+	void Flip(){
+		player.FacingRight = !player.FacingRight;
+		player.MoveSpeed *= -1;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+		rigidB2D.MoveRotation(rigidB2D.rotation + player.MoveSpeed * Time.fixedDeltaTime);
+
+
 	}
 
 	void Jump(){
-	    rigidB2D.velocity = new Vector2 (rigidB2D.velocity.x, player.JumpHeight);
+		animator.SetBool("Grounded", false);
+		rigidB2D.AddForce(new Vector2(0, player.JumpHeight));
+	    //rigidB2D.velocity = new Vector2 (rigidB2D.velocity.x, player.JumpHeight);
+		Debug.Log("Jumping at - " + rigidB2D.velocity.y);
+
+
+
 	}
 
 	void DoubleJump(){
 	    rigidB2D.velocity = new Vector2 (rigidB2D.velocity.x, player.JumpHeight);
 	    doubleJumped = true;
+	}
+
+	//Metodo para controlar jogador usando teclas (botao no UI) 
+//	void FixedUpdate(){
+//		float move = Input.GetAxis ("Horizontal");
+//		animator.SetFloat("Speed", Mathf.Abs(move));
+//		rigidB2D.velocity = new Vector2(move * player.MoveSpeed, rigidB2D.velocity.y);
+//
+//		if(move > 0 && !player.FacingRight){
+//			Flip();
+//		}else if (move < 0 && player.FacingRight){
+//			Flip();
+//		}
+//	}
+
+	//Metodo que executa automaticamente
+	void FixedUpdate(){
+		float move = rigidB2D.velocity.x;
+		animator.SetFloat("Speed", move);
+
+	    grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+	    animator.SetBool("Grounded", grounded);
+
+		if(grounded)
+	        doubleJumped = false;
+
+	   // animator.SetFloat("vSpeed", rigidB2D.velocity.y);
+
+		rigidB2D.velocity = new Vector2(player.MoveSpeed, rigidB2D.velocity.y);
+
+
+		 
+
+		//if(!grounded) return;
+
+		animator.SetFloat("Speed", rigidB2D.velocity.x);
+	}
+
+	void OnTriggerEnter2D (Collider2D collider){
+	   var tag = collider.gameObject.tag; 
+	   // Debug.Log("Checked into the Shredder");
+		if (tag == "Wall"){
+			Flip();
+
+		}
 	}
 }
