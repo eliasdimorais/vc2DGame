76018ec3305c8f9917598e6f523fa4761aa0d205
Elias de M. Fernandes <eliasdemoraes@live.com>
2016-05-20 21:04:51 -0300

    Added right method to find the correct obj

--- NONDA/Assets/_Scripts/Controller/TouchController.cs
+++ NONDA/Assets/_Scripts/Controller/TouchController.cs
@@ -3,55 +3,37 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class TouchController : MonoBehaviour {
-	private bool touchedOn = false;
-	//public GameObject itemPrefab; 
-	//public GameObject[] enemyPrefab;
+	public Sprite mySprite;
+	public int touchCount;
+	public enum ItemType{BANANA, BREAD, APPLE, LETTUCE};
+	public ItemType itemType;
+	public uint points;
 
-    Vector3 touchPosWorld;
-    TouchPhase touchPhase = TouchPhase.Ended;
-	public Touch tuch;
-	public Animator MyAnimator {
-		get;
-		private set;
+	void Awake () {
+		gameObject.GetComponent<SpriteRenderer>().sprite = mySprite;
+		Debug.Log(touchCount);
 	}
-    	
-	void Start () {
-//		enemyPrefab = GameObject.FindWithTag("Enemy");
-		//itemPrefab = GameObject.FindGameObjectsWithTag("Item");
-	}
-
-    void Update() {
-
-    	if(touchedOn){
-			Debug.Log(gameObject.name);	
-    		//verificar o tipo do toque - se foi um item ou se foi um inimigo
-    		//UpdatePoints();
-    	 }
-//		if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase) {
-//			Vector3 touchPosition = Input.GetTouch(0).position;
-//			//Vector3 g = Camera.allCameras[0].ScreenToWorldPoint(touchPosition);
-//
-//			Vector2 touchPosWorld2D = new Vector2(g.x, g.y);
-//			//RaycastHit2D hitInformation = Physics2D.Raycast(transform.position, touchPosWorld2D);
-//			//RaycastHit hitInfo = Camera.allCameras[0].ScreenPointToRay(touchPosition);
-//			Ray rayhit = Camera.allCameras[0].ScreenPointToRay(touchPosition);
-//		
-//			Debug.Log(rayhit);
-//			if (Physics.Raycast(rayhit, hit){
-//				Debug.Log(GameObject.Find(hit));
-//			}
-//		}
-    }
 
 	void OnMouseDown () {
-		touchedOn = true;
-		if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount != 0)
-		   	{	
-				Debug.Log(gameObject.name);
-				ItemController.Destroy(gameObject);
-
-		   }
+		touchCount--;
+		Debug.Log(touchCount);
+		if(touchCount <= 0){
+			GameManager.Instance.UpdateScore(points);
+			DestroyObject(gameObject);
+			switch(itemType){
+				case ItemType.BANANA:
+					
+					break;
+				case ItemType.BREAD:
+					break;
+				case ItemType.APPLE:
+					break;
+				case ItemType.LETTUCE:
+					break;	
+			}
+		}
+//		Debug.Log("Entrou aqui "  + gameObject.name); //NAO DELETAR
+//		ItemController.DestroyObject(gameObject);	 //NAO DELETAR
 	}
-
  }		
 
